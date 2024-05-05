a12cee6d5ad58b35aa385afa5e652b249afb056b
root <bereketzeselassie@gmail.com>
2024-05-05 12:52:57 +0000

    update

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List
+'''simple heper pagination '''
+
+
+def index_range(page, page_size):
+    ''' return tuple '''
+    a = page * page_size
+    b = a - page_size
+    return (b, a)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            ''' return list of list of csv '''
+            g = Server.dataset(self)
+            #ls = []
+            #for i in self.__dataset:
+             #   ls.append(i)
+            return(type(g))
