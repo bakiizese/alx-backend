5e198ddb3c80ac2c4176ab4370a38fa6789dc0aa
root <bereketzeselassie@gmail.com>
2024-05-14 14:25:04 +0000

    try

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -25,7 +25,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index() -> str:
     ''' route to / '''
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
