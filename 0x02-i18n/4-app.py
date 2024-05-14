957e666a97593350642e5c9a152281aa42aac70a
root <bereketzeselassie@gmail.com>
2024-05-14 15:38:29 +0000

    locales

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''  simple flask app '''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    ''' class '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    ''' get locale '''
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    ''' route to / '''
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=1)
