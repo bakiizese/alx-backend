33ea7212671947a7c14a09e16636e69377c920e9
root <bereketzeselassie@gmail.com>
2024-05-14 12:23:04 +0000

    setup babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''  simple flask app '''
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    ''' route to / '''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=1)
