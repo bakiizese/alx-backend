1bcbae0791ad76c563dc754b2e58eb48e8fffa50
root <bereketzeselassie@gmail.com>
2024-05-14 11:45:43 +0000

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index_home():
+def index():
+    ''' route to / '''
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5001)
