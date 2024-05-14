26d822543b6ebd4eb753a19d558fa77b196f65aa
root <bereketzeselassie@gmail.com>
2024-05-14 12:10:14 +0000

    fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,10 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
     ''' route to / '''
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=1)
