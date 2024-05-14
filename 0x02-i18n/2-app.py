cda49cc13f465f887a4229337f3cf21977aba6ac
root <bereketzeselassie@gmail.com>
2024-05-14 12:45:53 +0000

    supported langs

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,7 +18,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     ''' get locale '''
-    return request.accept_languages.best_match(['en', 'fr'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
